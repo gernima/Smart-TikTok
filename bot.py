3f82cd61717ba4b8abf672bb7e08a090ba9a136e
Max <braer.maks@gmail.com>
2021-07-19 12:30:56 +0300

    worked upload

--- bot.py
+++ bot.py
@@ -9,81 +9,114 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import NoSuchElementException
 from webdriver_manager.chrome import ChromeDriverManager as CM
-
+from video import *
+import re
+from collections import Counter
+import pickle
 
 
 class Bot:
     def __init__(self):
-        print('=====================================================================================================')
-        print('Heyy, you have to login manully on tiktok, so the bot will wait you 1 minute for loging in manually!')
-        print('=====================================================================================================')
-        time.sleep(8)
-        print('Running bot now, get ready and login manually...')
-        time.sleep(4)
-
         options = webdriver.ChromeOptions()
-        self.bot = webdriver.Chrome(options=options,  executable_path=CM().install())
-        self.bot.set_window_size(1680, 900)
+        self.driver = webdriver.Chrome(options=options,  executable_path=CM().install())
+        self.driver.set_window_size(1366, 768)
 
-        self.bot.get('https://www.tiktok.com/login')
-        ActionChains(self.bot).key_down(Keys.CONTROL).send_keys(
+        self.driver.get('https://www.tiktok.com/login')
+        ActionChains(self.driver).key_down(Keys.CONTROL).send_keys(
             '-').key_up(Keys.CONTROL).perform()
-        ActionChains(self.bot).key_down(Keys.CONTROL).send_keys(
+        ActionChains(self.driver).key_down(Keys.CONTROL).send_keys(
             '-').key_up(Keys.CONTROL).perform()
-        print('Waiting 50s for manual login...')
-        time.sleep(50)
-        self.bot.get('https://www.tiktok.com/upload/?lang=en')
+        if os.path.isfile('cookies.txt'):
+            self.load_cookie()
+        else:
+            print("please login, wait 30 seconds")
+            time.sleep(30)
+            self.save_cookie()
+        self.driver.get('https://www.tiktok.com/upload/?lang=en')
         time.sleep(3)
+        self.t = 60
+        self.n = 0
+        self.generate_time()
 
+        self.source = os.path.abspath("Videos/101dalmatinec/101dalmatinec.avi").replace("\\", "/")
+
+        self.tags = ["далматинцы", "рек", "rec", "мультик", "дисней", "момент", "нарезка", "101"]
+        self.description = "101 далматинец"
+        self.thread_name = "thread_1"
         # ================================================================
         # Here is the path of the video that you want to upload in tiktok.
         # Plese edit the path because this is different to everyone.
         # upload(r"C:/Users/redi/Videos/your-video-here.mov")
 
+    def generate_time(self):
+        self.t1 = self.n * self.t
+        self.t2 = (self.n + 1) * self.t
+
+    def save_cookie(self):
+        pickle.dump(self.driver.get_cookies() , open("cookies.txt","wb"))
+
+    def load_cookie(self):
+        cookies = pickle.load(open("cookies.txt", "rb"))
+        for cookie in cookies:
+            self.driver.add_cookie(cookie)
+
+    # def generate_tags(self, text):
+    #     text = text.lower()
+    #     words = re.findall(r'\w+', text)
+    #     word_counts = sorted(Counter(words), key=lambda x: len(x) >= )
+    #     i = 0
+    #     while len("#".join(self.tags)) <= (150 - len(self.description)):
 
     def check_exists_by_xpath(self, xpath):
         try:
-            driver.find_element_by_xpath(xpath)
+            self.driver.find_element_by_xpath(xpath)
         except NoSuchElementException:
             return False
 
         return True
 
-
-    def upload(self, video_path: string, tags: list):
-        file_uploader = self.bot.find_element_by_xpath(
+    def upload(self):
+        file_uploader = self.driver.find_element_by_xpath(
             '//*[@id="main"]/div[2]/div/div[2]/div[2]/div/div/input')
 
-        file_uploader.send_keys(video_path)
+        create_subclip(self.source, self.t1, self.t2, self.thread_name)
 
-        caption = self.bot.find_element_by_xpath(
+        self.n += 1
+        self.generate_time()
+
+        file_uploader.send_keys(os.path.abspath(f"Clips/{self.thread_name}/clip.mp4").replace("\\", "/"))
+
+        caption = self.driver.find_element_by_xpath(
             '//*[@id="main"]/div[2]/div/div[2]/div[3]/div[1]/div[1]/div[2]/div/div[1]/div/div/div/div/div/div/span')
 
-        self.bot.implicitly_wait(10)
-        ActionChains(self.bot).move_to_element(caption).click(
+        self.driver.implicitly_wait(10)
+        ActionChains(self.driver).move_to_element(caption).click(
             caption).perform()
-        # ActionChains(self.bot).key_down(Keys.CONTROL).send_keys(
+        # ActionChains(self.driver).key_down(Keys.CONTROL).send_keys(
         #     'v').key_up(Keys.CONTROL).perform()
-
-        for tag in tags:
-            ActionChains(self.bot).send_keys(tag).perform()
-            time.sleep(2)
-            ActionChains(self.bot).send_keys(Keys.RETURN).perform()
+        # self.generate_tags(video.get_text(video, thread_name))
+        for tag in self.tags:
+            ActionChains(self.driver).send_keys(" #" + tag).perform()
+            time.sleep(2.5)
+            ActionChains(self.driver).send_keys(Keys.RETURN).perform()
             time.sleep(1)
 
         time.sleep(5)
-        self.bot.execute_script("window.scrollTo(150, 300);")
+        self.driver.execute_script("window.scrollTo(150, 300);")
         time.sleep(5)
 
-        post = WebDriverWait(self.bot, 100).until(
-            EC.visibility_of_element_located(
-                (By.XPATH, '//*[@id="main"]/div[2]/div/div[2]/div[3]/div[5]/button[2]')))
+        # post = WebDriverWait(self.driver, 100).until(
+        #     EC.visibility_of_element_located(
+        #         (By.XPATH, '//*[@id="main"]/div[2]/div/div[2]/div[3]/div[6]/button[2]')))
 
-        post.click()
-        time.sleep(30)
-
-        if check_exists_by_xpath(self.bot, '//*[@id="portal-container"]/div/div/div[1]/div[2]'):
-            reupload = WebDriverWait(self.bot, 100).until(EC.visibility_of_element_located(
+        # post.click()
+        # time.sleep(30)
+        while self.check_exists_by_xpath('//*[@id="main"]/div[2]/div/div[2]/div[2]/div/div[1]/div[2]'):
+            pass
+        self.driver.find_element_by_xpath('//*[@id="main"]/div[2]/div/div[2]/div[3]/div[6]/button[2]').click()
+        print(f"{thread_name} loaded {self.source.split("/")[-1]} from {t1} to {t2}")
+        if self.check_exists_by_xpath(self.driver, '//*[@id="portal-container"]/div/div/div[1]/div[2]'):
+            reupload = WebDriverWait(self.driver, 100).until(EC.visibility_of_element_located(
                 (By.XPATH, '//*[@id="portal-container"]/div/div/div[1]/div[2]')))
 
             reupload.click()
@@ -93,13 +126,16 @@ class Bot:
                 time.sleep(600)
                 post.click()
                 time.sleep(15)
-                if check_exists_by_xpath(self.bot, '//*[@id="portal-container"]/div/div/div[1]/div[2]'):
+                if self.check_exists_by_xpath(self.driver, '//*[@id="portal-container"]/div/div/div[1]/div[2]'):
                     break
 
-        if check_exists_by_xpath(self.bot, '//*[@id="portal-container"]/div/div/div[1]/div[2]'):
-            reupload = WebDriverWait(self.bot, 100).until(EC.visibility_of_element_located(
+        if check_exists_by_xpath(self.driver, '//*[@id="portal-container"]/div/div/div[1]/div[2]'):
+            reupload = WebDriverWait(self.driver, 100).until(EC.visibility_of_element_located(
                 (By.XPATH, '//*[@id="portal-container"]/div/div/div[1]/div[2]')))
             reupload.click()
 
         time.sleep(1)
 
+bot = Bot()
+while True:
+    bot.upload()
--- video.py
+++ video.py
@@ -1,8 +1,12 @@
 from moviepy.video.io.ffmpeg_tools import ffmpeg_extract_subclip
 import os
+import speech_recognition
+from moviepy.editor import *
+from pathlib import Path
 
-video = os.path.abspath("Videos/101dalmatinec/101dalmatinec.avi").replace("\\", "/")
-print(video)
+
+# video = os.path.abspath("Videos/101dalmatinec/101dalmatinec.avi").replace("\\", "/")
+recognizer = speech_recognition.Recognizer()
 
 def get_duration(filename):
     result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
@@ -12,10 +16,22 @@ def get_duration(filename):
         stderr=subprocess.STDOUT)
     return float(result.stdout)
 
-def get_subcLip(video, t1, t2, thread_name):
-	return ffmpeg_extract_subclip(video, t1, t2, targetname=f"Clips/{thread_name}.avi")
+def create_subclip(video, t1, t2, thread_name):
+	Path(f"Clips/{thread_name}/").mkdir(parents=True, exist_ok=True)
+	VideoFileClip(video).subclip(t1, t2).write_videofile(f"Clips/{thread_name}/clip.mp4", fps=24, threads=10, codec="libx264")
 
 def delete_video(video):
 	os.remove(video)
 
-print(get_subcLip(video, 0, 180, "thread_1"))
+def get_text(video, thread_name):
+	clip = f"Clips/{thread_name}/clip.mp4"
+	wav_path = f"Clips/{thread_name}/audio.wav"
+	clip = VideoFileClip(clip) 
+	clip.audio.write_audiofile(wav_path)
+	audio = speech_recognition.AudioFile(wav_path)
+	with audio as source:
+		audio_file = recognizer.record(source)
+	return recognizer.recognize_google(audio_file, language="ru-RU")
+
+# create_subclip(video, 180*0, 180*1, "thread_1")
+# print(get_text(video, "thread_1"))
