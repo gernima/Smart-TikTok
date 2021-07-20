c7e0b619a1524cf1b3aeb7475f37ff43dafa50b5
Max <braer.maks@gmail.com>
2021-07-20 20:57:18 +0300

    working resize and adding text

--- video.py
+++ video.py
@@ -3,50 +3,68 @@ import os
 import speech_recognition
 from moviepy.editor import *
 from pathlib import Path
+import subprocess
 
-
-video = os.path.abspath("Videos/101dalmatinec/101dalmatinec.avi").replace("\\", "/")
+video = os.path.abspath("Videos/101dalmatinec.avi").replace("\\", "/")
 recognizer = speech_recognition.Recognizer()
+
 
 def get_duration(filename):
     result = subprocess.run(["ffprobe", "-v", "error", "-show_entries",
                              "format=duration", "-of",
                              "default=noprint_wrappers=1:nokey=1", filename],
-        stdout=subprocess.PIPE,
-        stderr=subprocess.STDOUT)
+                            stdout=subprocess.PIPE,
+                            stderr=subprocess.STDOUT)
     return float(result.stdout)
 
+
 def create_subclip(video, t1, t2, thread_name):
-	Path(f"Clips/{thread_name}/").mkdir(parents=True, exist_ok=True)
-	VideoFileClip(video).subclip(t1, t2).write_videofile(f"Clips/{thread_name}/clip.mp4", fps=24, threads=10, codec="libx264")
-
-def resize(video, width, height):
-	clip = VideoFileClip(video)
-	clip_resized = clip.resize(newsize=(clip.w, clip.h*2), apply_to_mask=False)
-	clip_resized.write_videofile(video)
+    Path(f"Clips/{thread_name}/").mkdir(parents=True, exist_ok=True)
+    clip = VideoFileClip(video).subclip(t1, t2)
+    clip.write_videofile(f"Clips/{thread_name}/clip.mp4", fps=24, threads=20, codec="libx264")
+    clip.reader.close()
+    clip.audio.reader.close_proc()
 
 
-def add_text(video, text, font="Calibri", color="white", fontsize=30):
-	my_video = VideoFileClip(video)
-	my_text = TextClip(text, font=font, color=color, fontsize=fontsize)
-	txt_col = my_text.on_color(size=(my_video.w + my_text.w, my_text.h+5), color=(0,0,0), pos=(6,'center'), col_opacity=0.6)
-	txt_mov = txt_col.set_pos( lambda t: (max(w/30,int(w-0.5*w*t)),max(5*h/6,int(100*t))))
-	final = CompositeVideoClip([my_video,txt_mov])
-	final.write_videofile(video, fps=24,codec='libx264')
+def resize(video, width=1080, height=1920):
+    clip = VideoFileClip(video)
+    size = int(clip.h * 1.1 // 2)
+    clip_with_borders = clip.margin(top=size, bottom=size)
+    clip_with_borders.write_videofile(video, fps=24, threads=20, codec="libx264")
+    clip.reader.close()
+    clip.audio.reader.close_proc()
+
+
+def add_text(video, text, font="Calibri", color="white", fontsize=50):
+    clip = VideoFileClip(video)
+    # Generate a text clip
+    txt_clip = TextClip(text, fontsize=fontsize, color=color, font=font)
+    # setting position of text in the center and duration will be 10 seconds
+    txt_clip = txt_clip.set_position(("center", clip.h * 0.2)).set_duration(clip.duration)
+    # Overlay the text clip on the first video clip
+    final = CompositeVideoClip([clip, txt_clip])
+    final.write_videofile(video, fps=24, threads=20, codec="libx264")
+    clip.reader.close()
+    clip.audio.reader.close_proc()
+
 
 def delete_video(video):
-	os.remove(video)
+    os.remove(video)
+
 
 def get_text(video, thread_name):
-	clip = f"Clips/{thread_name}/clip.mp4"
-	wav_path = f"Clips/{thread_name}/audio.wav"
-	clip = VideoFileClip(clip) 
-	clip.audio.write_audiofile(wav_path)
-	audio = speech_recognition.AudioFile(wav_path)
-	with audio as source:
-		audio_file = recognizer.record(source)
-	return recognizer.recognize_google(audio_file, language="ru-RU")
+    clip = f"Clips/{thread_name}/clip.mp4"
+    wav_path = f"Clips/{thread_name}/audio.wav"
+    clip = VideoFileClip(clip)
+    clip.audio.write_audiofile(wav_path)
+    audio = speech_recognition.AudioFile(wav_path)
+    with audio as source:
+        audio_file = recognizer.record(source)
+    return recognizer.recognize_google(audio_file, language="ru-RU")
 
-create_subclip(video, 5*10, 5*11, "thread_1")
-resize(f"Clips/thread_1/clip.mp4", 1080, 1920)
+
+t = 30
+create_subclip(video, t * 10, t * 11, "thread_1")
+resize(f"Clips/thread_1/clip.mp4")
+add_text(f"Clips/thread_1/clip.mp4", "Part #")
 # print(get_text(video, "thread_1"))
